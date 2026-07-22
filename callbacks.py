321013f7d3ed9c8274567786605faef90d1e971b
williampsun <williampsun07@gmail.com>
2026-07-22 16:12:26 -0400

    Move to top

--- callbacks.py
+++ callbacks.py
@@ -1,6 +1,6 @@
 '''
 Registers the app's single callback, which reacts to county/threshold/button changes and
-computes the charts + color-coded exposure summary shown in the result section.
+computes the charts+color-coded exposure summary shown in the result section.
 '''
 
 import numpy as np
